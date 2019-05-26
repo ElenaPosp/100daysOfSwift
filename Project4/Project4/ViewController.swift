d79e7aed4966c5edc6f8eb549d968851b379a840
ElenaPosp <pospelova_elena_al@mail.ru>
2019-05-26 17:12:11 +0500

    Update proj 4 with challange from day26

--- Project4/Project4/ViewController.swift
+++ Project4/Project4/ViewController.swift
@@ -10,30 +10,29 @@ import UIKit
 import WebKit
 
 
-class ViewController: UIViewController, WKNavigationDelegate {
+class ViewController: UIViewController {
 
     private var webView: WKWebView!
     var progressView: UIProgressView!
-    let webSites = ["apple.com", "hackingwithswift.com"]
+    var tableView: UITableView!
+    let webSites = ["apple.com", "hackingwithswift.com", "translate.google.com"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavBar()
         setupWebView()
+        setupTableView()
         webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
     }
 
     private func setupWebView() {
         webView = WKWebView()
         webView.navigationDelegate = self
-        let url = URL(string: "https://" + webSites.first!)!
-        webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
-        view = webView
     }
 
     private func setupNavBar() {
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+//        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
 
         let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
         let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
@@ -42,35 +41,44 @@ class ViewController: UIViewController, WKNavigationDelegate {
         progressView.sizeToFit()
         let progressButton = UIBarButtonItem(customView: progressView)
         
-        toolbarItems = [progressButton, spacer, refresh]
+        let back = UIBarButtonItem(barButtonSystemItem: .undo, target: webView, action: #selector(webView.goForward))
+        let next = UIBarButtonItem(barButtonSystemItem: .redo, target: webView, action: #selector(webView.goBack))
+        let cancel = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(dismissWebView))
+        toolbarItems = [cancel,progressButton, spacer, back, next, refresh]
         navigationController?.isToolbarHidden = false
     }
 
-    @objc func openTapped() {
-        let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
-        for webSite in webSites {
-            ac.addAction(UIAlertAction(title: webSite, style: .default, handler: openPage))
-        }
-        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
-        ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
-        present(ac, animated: true)
-    }
+//    @objc func openTapped() {
+//        let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
+//        ac.addAction(UIAlertAction(title: "vk.com", style: .default, handler: openPage))
+//        for webSite in webSites {
+//            ac.addAction(UIAlertAction(title: webSite, style: .default, handler: openPage))
+//        }
+//        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+//        ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+//        present(ac, animated: true)
+//    }
     
-    private func openPage(action: UIAlertAction) {
-        guard let title = action.title else { return }
-        guard let url = URL(string: "https://" + title) else { return }
-        webView.load(URLRequest(url: url))
-    }
-    
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        title = webView.title
-    }
-
     override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
         guard keyPath == "estimatedProgress" else { return }
         progressView.progress = Float(webView.estimatedProgress)
     }
 
+    private func setupTableView() {
+        tableView = UITableView(frame: view.frame, style: .plain)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: UITableViewCell.description())
+        view.addSubview(tableView)
+    }
+    
+    @objc func dismissWebView() {
+        view = tableView
+        title = ""
+    }
+}
+
+extension ViewController: WKNavigationDelegate {
     func webView(_ : WKWebView,
                  decidePolicyFor navigationAction: WKNavigationAction,
                  decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
@@ -86,6 +94,33 @@ class ViewController: UIViewController, WKNavigationDelegate {
                 return
             }
         }
+
+        let ac = UIAlertController(title: "This site dont allowed", message: nil, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Ok", style: .cancel))
+        present(ac, animated: true)
+        decisionHandler(.cancel)
+    }
+
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
     }
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return webSites.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: UITableViewCell.description(), for: indexPath)
+        cell.textLabel?.text = webSites[indexPath.row]
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let title = webSites[indexPath.row]
+        guard let url = URL(string: "https://" + title) else { return }
+        webView.load(URLRequest(url: url))
+        view = webView
+    }
+}
