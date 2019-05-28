af423a112205550359b6d5c29794e81f15f44669
ElenaPosp <pospelova_elena_al@mail.ru>
2019-05-29 00:50:12 +0500

    Progressup proj5

--- Project5/Project5/ViewController.swift
+++ Project5/Project5/ViewController.swift
@@ -51,8 +51,49 @@ class ViewController: UITableViewController {
     }
     
     private func submit(_ answer: String) {
+        let errTitle: String
+//        let errMessage: String
+        let lowerAnser = answer.lowercased()
+        if isPossible(word: lowerAnser){
+            if isOriginal(word: lowerAnser) {
+                if isReal(word: lowerAnser) {
+                    usedWords.insert(answer, at: 0)
+                    let indexPath = IndexPath(row: 0, section: 0)
+                    tableView.insertRows(at: [indexPath], with: .automatic)
+                    return
+                } else { errTitle = "Not real" }
+            } else { errTitle = "Not original" }
+        } else { errTitle = "Not possible" }
+        
+        let ac = UIAlertController(title: errTitle, message: nil, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Ok", style: .default))
+        present(ac, animated: true)
         
     }
+    
+    private func isPossible(word: String) -> Bool {
+        guard var tempWord = title?.lowercased() else { return false }
+        for letter in word {
+            if let position = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: position)
+            } else { return false }
+        }
+        return true
+    }
+    
+    private func isOriginal(word: String) -> Bool {
+        return !usedWords.contains(word)
+    }
+
+    private func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range,
+                                                            startingAt: 0, wrap: false, language: "en")
+        return misspelledRange.location == NSNotFound
+        
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return usedWords.count
     }
