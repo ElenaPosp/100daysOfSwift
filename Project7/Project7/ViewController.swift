73b3e6ef9675fc06682d183d76a7e15d809aefa8
ElenaPosp <pospelova_elena_al@mail.ru>
2019-06-04 23:32:22 +0500

    Update p7

--- Project7/Project7/ViewController.swift
+++ Project7/Project7/ViewController.swift
@@ -14,17 +14,31 @@ class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
-        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
         
+        let urlString: String
+        if tabBarController?.tabBarItem.tag == 0 {
+        // let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+        // urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+
+        }
         if let url = URL(string: urlString) {
             if let data = try? Data(contentsOf: url) {
                 parse(json: data)
+                return
             }
         }
+        showError()
+        
     }
     
+    private func showError() {
+        let ac = UIAlertController(title: "Loading error", message: nil, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Ok", style: .cancel))
+        present(ac, animated: true)
+    }
     private func parse(json: Data) {
         let decider = JSONDecoder()
         
@@ -44,6 +58,12 @@ class ViewController: UITableViewController {
         cell.textLabel?.text = petition.title
         cell.detailTextLabel?.text = petition.body
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.detailItem = petitions[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
     }
 }
 
