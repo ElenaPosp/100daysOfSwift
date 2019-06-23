2073b6f6a4312a80195d317ead756c9b49854ef8
ElenaPosp <pospelova_elena_al@mail.ru>
2019-06-23 20:37:29 +0500

    Progress up

--- Project11/Project11/GameScene.swift
+++ Project11/Project11/GameScene.swift
@@ -8,21 +8,42 @@
 
 import SpriteKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
 
+    var scoreLabel: SKLabelNode!
+    var score: Int = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "background")
         background.position = CGPoint(x: 512, y: 384)
         background.blendMode = .replace
         background.zPosition = -1
         addChild(background)
+
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.text = "Score: 0"
+        scoreLabel.horizontalAlignmentMode = .right
+        scoreLabel.position = CGPoint(x: 980, y: 700)
+        addChild(scoreLabel)
+
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
-        
+        physicsWorld.contactDelegate = self
+
+        makeSlot(at: CGPoint(x: 128, y: 0), isGood: true)
+        makeSlot(at: CGPoint(x: 384, y: 0), isGood: false)
+        makeSlot(at: CGPoint(x: 640, y: 0), isGood: true)
+        makeSlot(at: CGPoint(x: 896, y: 0), isGood: false)
+
         makeBouncer(at: CGPoint(x: 0, y: 0))
-        makeBouncer(at: CGPoint(x: 215, y: 0))
-        makeBouncer(at: CGPoint(x: 500, y: 0))
-        makeBouncer(at: CGPoint(x: 740, y: 0))
-        makeBouncer(at: CGPoint(x: 1000, y: 0))
+        makeBouncer(at: CGPoint(x: 256, y: 0))
+        makeBouncer(at: CGPoint(x: 512, y: 0))
+        makeBouncer(at: CGPoint(x: 768, y: 0))
+        makeBouncer(at: CGPoint(x: 1024, y: 0))
+        
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -31,7 +52,9 @@ class GameScene: SKScene {
         let ball = SKSpriteNode(imageNamed: "ballRed")
         ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width/2)
         ball.physicsBody?.restitution = 0.4
+        ball.physicsBody?.contactTestBitMask = ball.physicsBody?.collisionBitMask ?? 0
         ball.position = location
+        ball.name = "ball"
         addChild(ball)
     
     }
@@ -42,6 +65,57 @@ class GameScene: SKScene {
         bounser.physicsBody = SKPhysicsBody(circleOfRadius: bounser.size.width/2)
         bounser.physicsBody?.isDynamic = false
         addChild(bounser)
+    }
+    
+    private func makeSlot(at position: CGPoint, isGood: Bool) {
+        var slotBase: SKSpriteNode
+        var slotGlow: SKSpriteNode
+        if isGood {
+            slotBase = SKSpriteNode(imageNamed: "slotBaseGood")
+            slotGlow = SKSpriteNode(imageNamed: "slotGlowGood")
+            slotBase.name = "good"
+        } else {
+            slotBase = SKSpriteNode(imageNamed: "slotBaseBad")
+            slotGlow = SKSpriteNode(imageNamed: "slotGlowBad")
+            slotBase.name = "bad"
+        }
+        
+        slotBase.position = position
+        slotGlow.position = position
+        
+        slotBase.physicsBody = SKPhysicsBody(rectangleOf: slotBase.size)
+        slotBase.physicsBody?.isDynamic = false
+        addChild(slotBase)
+        addChild(slotGlow)
+        
+        let spin = SKAction.rotate(byAngle: .pi, duration: 10)
+        let spinForever = SKAction.repeatForever(spin)
+        slotGlow.run(spinForever)
+        
+    }
+    
+    func collision(ball: SKNode, object: SKNode) {
+        if object.name == "good" {
+            destroy(obj: ball)
+        } else if object.name == "bad" {
+            destroy(obj: ball)
+        }
+    }
+    
+    func destroy(obj: SKNode) {
+        obj.removeFromParent()
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        guard
+            let nodeA = contact.bodyA.node,
+            let nodeB = contact.bodyB.node
+        else { return }
+        if nodeA.name == "ball" {
+            collision(ball: nodeA, object: nodeB)
+        } else if nodeB.name == "ball" {
+            collision(ball: nodeB, object: nodeA)
+        }
     }
 }
 
