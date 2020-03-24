16f6d963d0e696adb51d28f53d520935611deab8
ElenaPosp <pospelova_elena_al@mail.ru>
2020-03-25 00:39:11 +0500

    Create 13 project

---
+++ Project13/Project13/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Project13
+//
+//  Created by Елена Поспелова on 24/03/2020.
+//  Copyright © 2020 ---. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var intensitySlider: UISlider!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func didTapChangeFilter() {
+    }
+    
+    @IBAction func didTapSave() {
+    }
+    @IBAction func intensityChanged() {
+    }
+    
+}
+
